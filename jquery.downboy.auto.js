a3a5d9a9c0b54541d2d28729d8040d1c7432f27b
Lewis Goddard <lewisgoddard@eustasy.org>
2017-05-01 21:56:38 +0100

    Update code style

--- jquery.downboy.auto.js
+++ jquery.downboy.auto.js
@@ -1,15 +1,17 @@
 // jQuery.downBoy.auto 1.2
+/*global $*/
+/* exported downBoy */
 function downBoy(footer) { // Define a new function
-	'use strict'; // Be strict
-	footer = footer || 'footer'; // Set $footer to Itself, OR footer
-	$(footer).css('position', '').css('bottom', ''); // Clear for measurement
+	'use strict' // Be strict
+	footer = footer || 'footer' // Set $footer to Itself, OR footer
+	$(footer).css('position', '').css('bottom', '') // Clear for measurement
 	if ( $(window).height() > $('body').height() ) { // Check if it's needed
-		$(footer).css('position', 'absolute').css('bottom', '0'); // Absolutely Position it
-	};
-};
+		$(footer).css('position', 'absolute').css('bottom', '0') // Absolutely Position it
+	}
+}
 $(function() {
-	downBoy(); // Run on load
+	downBoy() // Run on load
 	window.onresize = function() { // On Resize
-		downBoy(); // Run Again
-	};
-});
+		downBoy() // Run Again
+	}
+})
